9e934850f298377f8f105ea7eb991bd5842439a5
ctrlLey <leyna.dinh23@sitechhs.com>
2021-05-12 00:43:14 -0400

    well... its something
    bare minimum bc im tired rn. anyways i gotta touch up the scss and figure out wtf i wanna do with the rest of the site

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,5 @@
 const DOMSelectors = {
-  grid: document.querySelector(".movie-grid"),
+  tempBtn: document.querySelector(".bad-button"),
 };
 
 export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,25 @@
 import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
 
-const key = "YOURKEYHERE";
+const key = "9e4e20197f7246dc982ddf51354c09fd";
+
+// literally just copying the videos rn
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/random?number=1&apiKey=${key}`
+    );
+    const data = await response.json();
+    console.log(data);
+    // give title and a few details
+    // i really gotta get some classes goin bc this is a mess of class="intro"
+    // ill prob walk through like ten different approaches to a problem only to have it caused by mispelling "recipes". pain
+    DOMSelectors.tempBtn.insertAdjacentHTML(
+      "afterend",
+      `<p class="intro">${data.recipes[0].title}</p> <p class="intro">Ready In: ${data.recipes[0].readyInMinutes} minutes</p> <p class="intro">Vegan: ${data.recipes[0].vegan}</p> <p class="intro">Vegetarian: ${data.recipes[0].vegetarian}</p> <p class="intro"># of Ingredients: ${data.recipes[0].extendedIngredients.length}</p>`
+    );
+  } catch (error) {
+    console.log(error);
+    alert("fuck. i probably hit my quota lmao. plz stop refreshing");
+  }
+};
+query();
